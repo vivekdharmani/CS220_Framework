50d955b6d48c1d0ccf296516f4688c0b767507e8
vivekdharmani <vdhar001@ucr.edu>
2014-12-05 00:29:56 -0800

    Create lee.h
    Header file for Lee's Algorithm.

---
+++ Headers/lee.h
@@ -0,0 +1,37 @@
+/*
+ * lee.h
+ *
+ *  Created on: Nov 21, 2014
+ */
+
+#include<iostream>
+#include<string>
+#include "problem_object.h"
+#include "point.h"
+
+
+
+namespace Utilities{
+    class Lees {
+        private:
+    	int width,height;
+    	std::vector<Point> source, dest;
+        std::vector<Blocker> blocks;
+        
+        public:
+        std::vector< std::vector<Point> > paths;
+        Lees(ProblemObject* first_problem);  
+   
+    	~Lees(){
+            
+        };
+
+        void handle();
+   	void LeesBasic();
+        void Lees3bit();
+       	void Lees2Bit();
+
+    };
+
+
+}
